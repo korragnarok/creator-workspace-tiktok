6c5e4dd3b3f0617ee18bfbc52c02f05389f9e58d
korragnarok <103225573+korragnarok@users.noreply.github.com>
2026-06-05 23:17:50 -0400

    jj

--- layout.js
+++ layout.js
@@ -9,7 +9,8 @@
     { href: 'sales-calendar.html', label: 'Sales Log', mobileLabel: 'Sales', icon: 'sales' }
   ];
 
-  const MOBILE_ITEMS = NAV_ITEMS.filter(item =>
+  // All pages in mobile nav
+  const MOBILE_ITEMS = NAV_ITEMS; // was: NAV_ITEMS.filter(item =>
     ['index.html', 'daily-todo.html', 'sales-calendar.html', 'products.html'].includes(item.href)
   );
 
