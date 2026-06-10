806b585cbed95ba1bee20c6f237fd00a5fa34b70
korragnarok <103225573+korragnarok@users.noreply.github.com>
2026-06-09 21:38:37 -0400

    asd

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ─── Creator Hub Service Worker ───────────────────────────────────────────────
-const CACHE = 'creator-hub-v76';
+const CACHE = 'creator-hub-v77';
 
 // Core files to cache for offline shell
 const PRECACHE = [
