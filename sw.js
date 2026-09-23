ec59c1fc549825a573849f2d7878b3bf7ef36e4a
korragnarok <103225573+korragnarok@users.noreply.github.com>
2026-09-23 19:36:04 -0400

    rearrange crds

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ─── Creator Hub Service Worker ───────────────────────────────────────────────
-const CACHE = 'creator-hub-v162';
+const CACHE = 'creator-hub-v164';
 
 // Core files to cache for offline shell
 const PRECACHE = [
